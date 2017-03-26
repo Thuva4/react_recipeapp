6f6d37f2cab791136e588667892494188826c9aa
thuva4 <tharma.thuva@gmail.com>
2017-03-26 21:29:21 +0530

    edit main component

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,14 +1,19 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Nav = require('./nav.jsx');
+var Footer = require('./footer.jsx');
 
 var Main = React.createClass(
 {
 	render: function(){
 		return(
 			<div className='Well'>
+			<Nav></Nav>
+
 			{this.props.children}
 			<br></br>
 			<br></br>
+<Footer></Footer>
 			</div>
 		)
 	}
