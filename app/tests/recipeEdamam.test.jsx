e3e772a3815efd78574503bddf8b0b5511eabdc2
thuva4 <tharma.thuva@gmail.com>
2017-03-28 01:50:57 +0530

    add recipeEdamamAPI component test

---
+++ app/tests/recipeEdamam.test.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jquery');
+var TestUtils = require('react-addons-test-utils');
+
+var RecipeListEdamam = require('app/components/RecipeListEdamam.jsx');
+
+describe('RecipeListEdamam', () => {
+  it('should exist', () => {
+    expect(RecipeListEdamam).toExist();
+  });
+});
